0d2dd851a7c3666fa4bf9ee032058ac1d6557593
Alexander Sandberg <alexandersandbergnystart@gmail.com>
2026-08-24 21:19:26 +0200

    inte klar börjadet skapa databas ej klar

--- database/main.sql
+++ database/main.sql
@@ -1,1 +1,16 @@
-create database Alexanderhomepage
+create database Alexanderhomepage;
+USE Alexanderhomepage;
+
+CREATE TABLE quiz  (
+svar varchar(255),
+frågor varchar(255),
+antal_rätt INT DEFAULT 0
+);
+INSERT INTO quiz (frågor, svar)
+VALUES
+('Vad heter Sveriges huvudstad?', 'Stockholm'),
+('Hur många dagar har en vecka?', '7'),
+('Vad är 2 + 2?', '4');
+
+SELECT * FROM quiz;
+
--- svar.php
+++ svar.php
@@ -1,24 +1,5 @@
  
 <?php
-
-if ($_SERVER["REQUEST_METHOD"] !== "POST") {
-    exit("Ogiltig förfrågan.");
-}
-$poäng =0;
-$max_poäng=1;
-$rätta_svar=[  
-    "fraga1" => "blå",
-    "fraga2" => "grön"
-];
-for ($i = 1; $i <= count($rätta_svar); $i++) {
-
-    $fråga = "fraga" . $i;
-
-    if (isset($_POST[$fråga]) && $_POST[$fråga] === $rätta_svar[$fråga]) {
-        $poäng++;
-    }
-}
-
-echo $poäng . " av " . $max_poäng . " rätt";
+ 
 ?>
  
